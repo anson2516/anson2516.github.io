ce9f33b3ee2eb9583fa1bb125c2cf0872fc736ca
anson2516 <2516529973@qq.com>
2021-10-04 10:47:41 +0800

    update weather Responsive/API URL

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
 const temperature = document.querySelector('#temperature')
 const weatherIcon = document.querySelector('#weatherIcon')
+const now = document.querySelector('#now')
 
-
+//fetch weather & date
 const getResult = async() => {
-    const res = await fetch('https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=flw&lang=tc')
+    const res = await fetch('https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread&lang=tc')
     const data = await res.json()
-    console.log(data)
+    now.innerHTML = data.updateTime.slice(0,10)
     return data
 }
 
 getResult()
 .then((data)=>{
-    const searchTerm = '度'
-    const indexOfFirst = data.forecastDesc.indexOf(searchTerm);
-    const indexofSecond = data.forecastDesc.indexOf(searchTerm, (indexOfFirst + 1));
-    const MinTemperature = data.forecastDesc[indexOfFirst-2]+data.forecastDesc[indexOfFirst-1]
-    const MaxTemperature = data.forecastDesc[indexofSecond-2]+data.forecastDesc[indexofSecond-1]
-    temperature.innerHTML = `${MinTemperature}  ~ ${MaxTemperature}。`
-    if(data.forecastDesc.includes("部分時間有陽光")){
-        weatherIcon.src = "./img/53.png" 
-    }
-    else if(data.forecastDesc.includes("大致天晴")){
-        weatherIcon.src = "./img/50.png"
-    }
-    else if(data.forecastDesc.includes("大致多雲")){
-        weatherIcon.src = "./img/60.png"
-    }else{
-        weatherIcon.src = "./img/51.png"
-    }
+    temperature.innerHTML = `${data.temperature.data[1].value}。C`
+    weatherIcon.src = `./img/weatherIcon/${data.icon[0]}.png`  
     
 })
 .catch(err => console.log(err))
