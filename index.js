2d2f4cf76fbd76449d2f332f401acf748cef1a1c
anson2516 <2516529973@qq.com>
2021-10-27 20:02:24 +0800

    Add Weather Website

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const temperature = document.querySelector('#temperature')
-const weatherIcon = document.querySelector('#weatherIcon')
-const now = document.querySelector('#now')
+// const temperature = document.querySelector('#temperature')
+// const weatherIcon = document.querySelector('#weatherIcon')
+// const now = document.querySelector('#now')
 
-//fetch weather & date
-const getResult = async() => {
-    const res = await fetch('https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread&lang=tc')
-    const data = await res.json()
-    now.innerHTML = data.updateTime.slice(0,10)
-    return data
-}
+// //fetch weather & date
+// const getResult = async() => {
+//     const res = await fetch('https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread&lang=tc')
+//     const data = await res.json()
+//     now.innerHTML = data.updateTime.slice(0,10)
+//     return data
+// }
 
-getResult()
-.then((data)=>{
-    temperature.innerHTML = `${data.temperature.data[1].value}。C`
-    weatherIcon.src = `./img/weatherIcon/${data.icon[0]}.png`  
+// getResult()
+// .then((data)=>{
+//     temperature.innerHTML = `${data.temperature.data[1].value}。C`
+//     weatherIcon.src = `./img/weatherIcon/${data.icon[0]}.png`  
     
-})
-.catch(err => console.log(err))
+// })
+// .catch(err => console.log(err))
