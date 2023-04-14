9b787ba6896cf0e808fe870ad1598a3d29afea3a
anson2516 <86390671+anson2516@users.noreply.github.com>
2023-04-15 02:52:56 +0800

    delete dir

--- codecademy_exercise/JS_Introduction_Kelvin_weather.js
+++
@@ -1,9 +0,0 @@
-// hahahah
-const kelvin=0;
-//yo
-const celsius = kelvin - 273;
-// f u 
-let fahrenheit = celsius * (9/5) + 32;
-//hfxfgf
-fahrenheit = Math.floor(fahrenheit);
-console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
